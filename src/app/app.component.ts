ad9c0edb4aeb18adc6ae5004f91a2fa91cd8146b
Fahd Allebdi <fallebdi@channels.com.sa>
2019-07-29 22:58:52 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup,Validators,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quickWhatsapp';
+  
+  form:FormGroup;
+
+  constructor(builder:FormBuilder){
+
+    this.form = builder.group({
+      'mobileNo': new FormControl('',[Validators.required]),
+      'text': new FormControl('',[Validators.required]),
+    });
+
+  }
+
+
+  onSubmit(){
+    const mobileNo = this.form.get('mobileNo').value;
+    const text = this.form.get('text').value;
+    this.openChat(mobileNo,text);
+    this.form.reset();
+  }
+
+  /**
+   * 
+   * @param mobileNo 
+   * Whatsapp Api https://wa.me/<number>/?text=urlencodedtext
+   */
+  private openChat(mobileNo:number,text:string){
+    text = encodeURI(text);
+    console.log(text);
+    let url = `https://wa.me/${mobileNo}/?text=${text}`;
+    // open url with new tab
+    window.open(url,'_blank');
+  }
 }
